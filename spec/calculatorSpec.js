b39a2461d68ccc8cb98893f63f8cab7ce7aa7602
Axel Aguilar <abc709@hotmail.com>
2016-01-23 16:51:02 +0100

    Setting up Jasmine project

---
+++ spec/calculatorSpec.js
@@ -0,0 +1,6 @@
+describe('calculator', function() {
+	it('should add two numbers', function(){
+		var resultat = addFn(1,2);
+		expect(resultat).toBe(3);
+	});
+})
---
+++ src/calculator.js
@@ -0,0 +1,3 @@
+function addFn(a, b) {
+	return a+b;
+}
